c85e8b0faf9fff7f4165020b34f8669a326ddc3e
draikos <jellevdtoren@hotmail.com>
2017-03-23 14:02:32 +0100

    test

---
+++ GUI/Data/ReadFile.py
@@ -0,0 +1,29 @@
+import sys
+from PyQt5.QtWidgets import QApplication
+from openpyxl import load_workbook
+
+
+class ReadFile:
+    def __init__(self):
+        super().__init__()
+
+        self.ReadFile()
+
+    def ReadFile(self):
+        wb = load_workbook(filename="test.xlsx", read_only=True)
+        ws = wb["Blad1"]
+
+
+
+
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    ex = ReadFile()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
