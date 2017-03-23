93cd57504629e49cd8cfb2861054d85cf25ffc65
draikos <jellevdtoren@hotmail.com>
2017-03-23 14:19:09 +0100

    original code

--- GUI/UI/Main.py
+++ GUI/UI/Main.py
@@ -21,20 +21,19 @@ class Ui_MainWindow(object):
         self.groupboxSetup()
         self.bottomLayout()
         self.leftLayout()
-        self.label2()
-        self.label1()
-        self.layoutRight()
+        self.rightLayoutPositioner()
+        self.leftLayoutPositioner()
+        self.rightLayout()
         self.mainLayout()
         self.menubar()
-
         MainWindow.setCentralWidget(self.centralWidget)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
+    # -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    #  Layout setup
 
     def layout(self):
-        self.gridLayout_2 = QtWidgets.QGridLayout()
-        self.gridLayout_5 = QtWidgets.QGridLayout()
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setAutoFillBackground(False)
         self.centralWidget.setObjectName("centralWidget")
@@ -49,101 +48,56 @@ class Ui_MainWindow(object):
 
 
     def bottomLayout(self):
-        self.gridLayout = QtWidgets.QGridLayout()
-        self.gridLayout.setContentsMargins(11, 9, 11, 11)
-        self.gridLayout.setSpacing(6)
-        self.gridLayout.setObjectName("bottomLayout")
-        self.gridLayout.addWidget(self.groupBox_3, 0, 0, 1, 1)
+        self.bottomLayout = QtWidgets.QGridLayout()
+        self.bottomLayout.setContentsMargins(11, 9, 11, 11)
+        self.bottomLayout.setSpacing(6)
+        self.bottomLayout.setObjectName("bottomLayout")
+        self.bottomLayout.addWidget(self.groupBox_3, 0, 0, 1, 1)
 
     def leftLayout(self):
-        self.gridLayout_2.setContentsMargins(11, 11, 11, 11)
-        self.gridLayout_2.setSpacing(20)
-        self.gridLayout_2.setObjectName("leftLayout")
-        self.gridLayout_2.addWidget(self.groupBox, 0, 0, 2, 1)
+        self.leftLayout = QtWidgets.QGridLayout()
+        self.leftLayout.setContentsMargins(11, 11, 11, 11)
+        self.leftLayout.setSpacing(20)
+        self.leftLayout.setObjectName("leftLayout")
+        self.leftLayout.addWidget(self.groupBox, 0, 0, 2, 1)
 
-    def label2(self):
-        self.gridLayout_3 = QtWidgets.QGridLayout(self.groupBox_2)
-        self.gridLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.gridLayout_3.totalHeightForWidth(200)
+    def rightLayoutPositioner(self):
+        self.rightLayoutPositioner = QtWidgets.QGridLayout(self.groupBox_2)
+        self.rightLayoutPositioner.setContentsMargins(100, 100, 100, 100)
         self.setupMappingVisualisation()
 
-
-
-
-
-    def setupMappingVisualisation(self):
-        v = 0
-        layout = self.gridLayout_3
-        indexSensors = 192
-        listV = []
-        listT = []
-        for t in range(indexSensors):
-            sensor = "sensor"+str(t)
-            listV.append(sensor)
-
-        i = 24
-        j = 8
-        for g in range(i):
-            if g == 0 or g == 23:
-                for t in range(j):
-                    if t == 0 or t == 7:
-                        listV[v] = QWidget()
-                        test = listV[v]
-                        v += 1
-                        listT.append(test)
-                        test.setFixedSize(20, 20)
-                        layout.addWidget(test, t, g)
-
-                    else:
-                        listV[v] = QWidget()
-                        test = listV[v]
-                        v += 1
-                        listT.append(test)
-                        test.setAutoFillBackground(True)
-                        test.setFixedSize(20, 20)
-                        test.setStyleSheet("background-color: red")
-                        layout.addWidget(test, t, g)
-            else:
-                for t in range(j):
-                    listV[v] = QWidget()
-                    test = listV[v]
-                    v += 1
-                    listT.append(test)
-                    test.setAutoFillBackground(True)
-                    test.setFixedSize(20,20)
-                    test.setStyleSheet("background-color: green")
-                    layout.addWidget(test, t, g)
-
-    def label1(self):
-        self.gridLayout_4 = QtWidgets.QGridLayout(self.groupBox)
-        self.gridLayout_4.setContentsMargins(11, 11, 11, 11)
-        self.gridLayout_4.setSpacing(6)
-        self.gridLayout_4.setObjectName("label1")
+    def leftLayoutPositioner(self):
+        self.leftLayoutPositioner = QtWidgets.QGridLayout(self.groupBox)
+        self.leftLayoutPositioner.setContentsMargins(11, 11, 11, 11)
+        self.leftLayoutPositioner.setSpacing(6)
+        self.leftLayoutPositioner.setObjectName("label1")
         self.label = QtWidgets.QLabel(self.groupBox)
         self.label.setObjectName("label")
-        self.gridLayout_4.addWidget(self.label, 0, 0, 1, 1)
+        self.leftLayoutPositioner.addWidget(self.label, 0, 0, 1, 1)
 
 
-    def layoutRight(self):
-        self.gridLayout_5.setContentsMargins(11, 11, 11, 11)
-        self.gridLayout_5.setSpacing(6)
-        self.gridLayout_5.setObjectName("layoutRight")
-        self.gridLayout_5.addWidget(self.groupBox_2, 0, 0, 1, 1)
+
+    def rightLayout(self):
+        self.rightLayout = QtWidgets.QGridLayout()
+        self.rightLayout.setContentsMargins(11, 11, 11, 11)
+        self.rightLayout.setSpacing(6)
+        self.rightLayout.setObjectName("layoutRight")
+        self.rightLayout.addWidget(self.groupBox_2, 0, 0, 1, 1)
 
     def mainLayout(self):
-        self.gridLayout_6 = QtWidgets.QGridLayout(self.centralWidget)
-        self.gridLayout_6.setContentsMargins(9, 11, 11, 0)
-        self.gridLayout_6.setHorizontalSpacing(15)
-        self.gridLayout_6.setVerticalSpacing(10)
-        self.gridLayout_6.setObjectName("mainLayout")
-        self.gridLayout_6.addLayout(self.gridLayout_2, 0, 0, 1, 1)
-        self.gridLayout_6.addLayout(self.gridLayout_5, 0, 1, 1, 1)
-        self.gridLayout_6.addLayout(self.gridLayout, 1, 0, 1, 2)
-        self.gridLayout_6.setColumnMinimumWidth(1, 2)
-        self.gridLayout_6.setColumnStretch(0, 1)
-        self.gridLayout_6.setColumnStretch(1, 2)
-        self.gridLayout_6.setRowStretch(0, 3)
-        self.gridLayout_6.setRowStretch(1, 1)
+        self.mainLayout = QtWidgets.QGridLayout(self.centralWidget)
+        self.mainLayout.setContentsMargins(9, 11, 11, 0)
+        self.mainLayout.setHorizontalSpacing(10)
+        self.mainLayout.setVerticalSpacing(10)
+        self.mainLayout.setObjectName("mainLayout")
+        self.mainLayout.addLayout(self.leftLayout, 0, 0, 1, 1)
+        self.mainLayout.addLayout(self.rightLayout, 0, 1, 1, 1)
+        self.mainLayout.addLayout(self.bottomLayout, 1, 0, 1, 2)
+        self.mainLayout.setColumnMinimumWidth(1, 2)
+        self.mainLayout.setColumnStretch(0, 1)
+        self.mainLayout.setColumnStretch(1, 2)
+        self.mainLayout.setRowStretch(0, 3)
+        self.mainLayout.setRowStretch(1, 1)
 
     def menubar(self):
         self.menuBar = QtWidgets.QMenuBar(MainWindow)
@@ -161,10 +115,64 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.groupBox.setTitle(_translate("MainWindow", "GroupBox"))
+        self.groupBox.setTitle(_translate("MainWindow", "GroupBox1"))
         self.label.setText(_translate("MainWindow", "dit is de eerste label"))
-        self.groupBox_2.setTitle(_translate("MainWindow", "GroupBox"))
-        self.groupBox_3.setTitle(_translate("MainWindow", "GroupBox"))
+        self.groupBox_2.setTitle(_translate("MainWindow", "GroupBox2"))
+        self.groupBox_3.setTitle(_translate("MainWindow", "GroupBox3"))
+
+    # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # visualisation setup
+    def setupMappingVisualisation(self):
+        v = 0
+        layout = self.rightLayoutPositioner
+        indexSensors = 192
+        listV = []
+        for t in range(indexSensors):
+            sensor = "sensor" + str(t)
+            listV.append(sensor)
+
+        i = 24
+        j = 8
+        for g in range(i):
+            if g == 0 or g == 23:
+                for t in range(j):
+                    if t == 0 or t == 7:
+                        listV[v] = QWidget()
+                        test = listV[v]
+                        v += 1
+                        test.setMinimumHeight(20)
+                        test.setMinimumWidth(20)
+                        test.setMaximumHeight(40)
+                        test.setMaximumWidth(40)
+                        layout.addWidget(test, t, g)
+
+
+                    else:
+                        listV[v] = QWidget()
+                        test = listV[v]
+                        v += 1
+                        test.setMinimumHeight(20)
+                        test.setMinimumWidth(20)
+                        test.setMaximumHeight(40)
+                        test.setMaximumWidth(40)
+                        test.setAutoFillBackground(True)
+                        test.setStyleSheet("background-color: red")
+                        layout.addWidget(test, t, g)
+            else:
+                for t in range(j):
+                    listV[v] = QWidget()
+                    test = listV[v]
+                    v += 1
+                    test.setMinimumHeight(20)
+                    test.setMinimumWidth(20)
+                    test.setMaximumHeight(60)
+                    test.setMaximumWidth(60)
+                    test.setAutoFillBackground(True)
+                    test.setStyleSheet("background-color: green")
+                    layout.addWidget(test, t, g)
+
+
+
 
 
 
