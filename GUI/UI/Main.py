2902f9546aba6ec023ca5e890c2149f70ce87681
draikos <jellevdtoren@hotmail.com>
2017-03-31 17:53:50 +0200

    signal slot phase

--- GUI/UI/Main.py
+++ GUI/UI/Main.py
@@ -2,6 +2,13 @@ import sys
 import time
 from collections import defaultdict
 
+import matplotlib
+# Make sure that we are using QT5
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+
 from PyQt5.QtWidgets import QDialog, QApplication, qApp
 from PyQt5.QtCore import Qt, QEvent, QThread, pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QMainWindow
@@ -10,27 +17,24 @@ from PyQt5.QtWidgets import QWidget
 from GUI.UI.Ui_MainWindow import Ui_MainWindow
 from GUI.Data import ReadFile
 
+
 class MyMainWindow(QMainWindow, Ui_MainWindow):
+    rowValue = pyqtSignal(int)
     def __init__(self, parent=None):
+
         super(MyMainWindow, self).__init__(parent)
         MyMainWindow.d = defaultdict(list)
         MyMainWindow.dictionary = {}
         self.setupUi(self)
         self.setupMappingVisualisation()
+
         self.show()
 
     def keyPressEvent(self, e):
 
         if e.key() == Qt.Key_Escape:
-
-            self.update = DataThread()
-            self.update.start()
-            self.update.progress.connect(self.paintWidget)
-
-
-    def paintWidget(self, x):
-        print(x)
-
+            self.update()
+            self.rowValue.connect(self.addmpl)
 
     def setupMappingVisualisation(self):
         layout = self.gridLayout_3
@@ -81,7 +85,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         object1 = ReadFile.ReadFile()
         limit = 0
         for rows in object1.ws.rows:
-            if limit <= 600:
+            if limit <= 100:
                 c = 0
                 for cell in rows:
                     self.d["dataSensor{0}".format(c)].append(cell.value)
@@ -91,23 +95,8 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             limit += 1
         print("--- %s seconds ---" % (time.time() - start_time))
 
-    #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    # updater
-
-class DataThread(QThread):
-    progress = pyqtSignal(int)
-
-    def __init__(self):
-        QThread.__init__(self)
-        self.d = MyMainWindow.d
-        self.dictionary = MyMainWindow.dictionary
-
-
-    def __del__(self):
-        self.wait()
-
-    @pyqtSlot()
     def update(self):
+
         FPS = 60
         counterSensor = 0
         counterValue = 0
@@ -117,6 +106,8 @@ class DataThread(QThread):
         test = self.d
         print("--- %s seconds ---" % (time.time() - start_time))
         for v in range(len(self.d.get("dataSensor0"))):
+            self.rowValue.emit(v)
+            QApplication.processEvents()
             while True:
                 currentTime = time.time()
                 sleepTime = 1. / FPS - (currentTime - lastFrameTime)
@@ -125,24 +116,47 @@ class DataThread(QThread):
                         if x == 0 or x == 7 or x == 184 or x == 191:
                             pass
                         else:
-                            print("test")
+
                             value = test.get("dataSensor{0}".format(x))[v]
-                            # widget = test2["widget{0}".format(x)].setStyleSheet(
-                            #     "background-color: rgb({0}, {0}, {0})".format(value * 150))
-                            self.progress.emit(x)
-                            DataThread.wait(1000)
-
-
-
+                            test2["widget{0}".format(x)].setStyleSheet(
+                                "background-color: rgb(244, {0}, {0})".format(value * 150))
+                            test2["widget{0}".format(x)].repaint()
                     break;
                 lastFrameTime = currentTime
+
         print("--- %s seconds ---" % (time.time() - start_time))
 
+    def addmpl(self, v):
+        print(v)
+
+        # fig1 = Figure()
+        # ax1f1 = fig1.add_subplot(111)
+        # value = self.d.get("dataSensor1")
+        # ax1f1.plot(value)
+        # self.canvas = FigureCanvas(fig1)
+        # self.gridLayout.addWidget(self.canvas)
+        # self.canvas.draw()
+
+
+
+
+
+class DataThread(QThread):
+    progress = pyqtSignal(int)
+
+    def __init__(self):
+        QThread.__init__(self)
+
+    def __del__(self):
+        self.wait()
+
     def run(self):
-        self.update()
+        print()
 
 
 if __name__ == '__main__':
+
+
     app = QApplication(sys.argv)
-    win = MyMainWindow()
+    main = MyMainWindow()
     sys.exit(app.exec_())
